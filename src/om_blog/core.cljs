188eb136826400353bae0e87018b33c40a1b2d75
WhiteCity First <wfirst@localhost.localdomain>
2016-03-31 15:44:41 +0300

    functionality done

--- src/om_blog/core.cljs
+++ src/om_blog/core.cljs
@@ -1,66 +1,85 @@
 (ns om-blog.core
-  (:require [om.core :as om :include-macros true]
-            [goog.events :as events]
-            [om-bootstrap.button :as b]
-            [om-bootstrap.modal :as md]
-            [cljs.core.async :refer [put! chan <!]]
-            [om.dom :as dom :include-macros true])
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:import [goog.net XhrIo]
-           goog.net.EventType
-           [goog.events EventType]))
+    (:require [cljsjs.markdown :as markdown]
+              [om.core :as om :include-macros true]
+              [goog.events :as events]
+              [om-bootstrap.button :as b]
+              [om-bootstrap.modal :as md]
+              [cljs.core.async :refer [put! chan <!]]
+              [om.dom :as dom :include-macros true])
+    (:require-macros [cljs.core.async.macros :refer [go]])
+    (:import [goog.net XhrIo]
+             goog.net.EventType
+             [goog.events EventType]))
 
 (enable-console-print!)
 
 (defn json-xhr [{:keys [method url data on-complete]}]
-  (let [xhr (XhrIo.)]
-    (events/listen xhr goog.net.EventType.COMPLETE
-      (fn [e]
-        (on-complete (js->clj (.getResponseJson xhr)))))
-    (. xhr
-      (send url method (when data (clj->js data))
-        #js {"Content-Type" "application/json"}))))
+    (let [xhr (XhrIo.)]
+        (events/listen xhr goog.net.EventType.COMPLETE
+            (fn [e]
+                (on-complete (js->clj (.getResponseJson xhr)))))
+                (. xhr
+                    (send url method (when data (clj->js data))
+                    #js {"Content-Type" "application/json"}))))
 
 (defn load-articles [url on-complete]
-  (json-xhr
-    {:method "GET"
-     :url url
-     :data nil
-     :on-complete on-complete}))
+    (json-xhr
+        {:method "GET"
+         :url url
+         :data nil
+         :on-complete on-complete}))
 
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:articles []
-                          :single-article []
                           :article-pages []
                           :page-number 1}))
 
-(defn get-single-article [page-id channel]
-    (load-articles (str "http://localhost:3000/blogposts/" page-id)
-    (fn [res]
-        (let [final-res {:type "single" :value res}]
-        (put! channel final-res)))))
-
-
 (defn reload-articles [page-num channel]
     (load-articles (str "http://localhost:3000/blogposts?page=" page-num)
-    (fn [res]
-        (put! channel res))))
+        (fn [res]
+            (put! channel res))))
+
+(defn trigger [article owner]
+    (reify
+        om/IInitState
+        (init-state [_]
+            {:visible? false :article []})
+        om/IRender
+        (render [_]
+            (dom/div #{:className "modal in"}
+                (md/modal {:header        (dom/h4 #js
+                                            {:dangerouslySetInnerHTML #js
+                                                {:__html (.toHTML js/markdown
+                                                            (str (get (om/get-state owner :article) "title")))}})
+                           :footer        (dom/div (b/button {} "Save")
+                                                    (b/button {:on-click
+                                                                (fn [_] (om/set-state! owner :visible? false))} "Close"))
+                           :close-button? false
+                           :visible?      (om/get-state owner :visible?)}
+                                          (dom/div #{:className "container"}
+                                            (dom/span #js {:dangerouslySetInnerHTML #js
+                                                            {:__html
+                                                                (.toHTML js/markdown
+                                                                    (str (get (om/get-state owner :article) "body")))}})))
+                                (b/button {:bs-style "primary"
+                                           :bs-size "large"
+                                           :on-click (fn [_] (do (om/set-state! owner :visible? true)
+                                                                 (load-articles (str "http://localhost:3000/blogposts/"
+                                                                                (str (get article "id")))
+                                                                    (fn [res]
+                                                                        (om/set-state! owner :article (js->clj res))))))}
+                                    "Read More")))))
 
 (defn article [article owner]
-  (reify
-    om/IRenderState
-    (render-state [state {:keys [current-page]}]
-      (dom/li nil
-        (apply dom/div nil
-          [(dom/h2 nil (get article "title"))
-           (dom/div nil (get article "body"))
-           (dom/button #js {:type "button"
-                            :class "singleArticleButton"
-                            :onClick #(get-single-article
-                                        (str (get article "id"))
-                                        current-page)}
-                            "Read More")])))))
+    (reify
+        om/IRenderState
+        (render-state [state {:keys [current-page]}]
+            (dom/li nil
+                (apply dom/div nil
+                    [(dom/h2 nil (get article "title"))
+                    (dom/div #js {:dangerouslySetInnerHTML #js {:__html (.toHTML js/markdown (str (get article "body")))}})
+                    (om/build trigger article)])))))
 
 (defn page-bar [state owner]
     (reify
@@ -74,45 +93,35 @@
                                       :onClick (fn [] (reload-articles (- % 1) current-page))} % )
                     (range 1 (+ 1 (:article-pages state)))))))))
 
-(defn single-article [state owner]
-    (reify
-        om/IRender
-        (render [_]
-            (dom/div #js {:className "container"}
-                (dom/h2 nil (get state "title"))
-                (dom/span nil (get state "body"))))))
-
 (defn articles [state owner]
-  (reify
-    om/IInitState
-    (init-state [_]
-        {:current-page (chan)})
-    om/IRenderState
-    (render-state [this {:keys [current-page]}]
-        (dom/div #js {:className "container"}
-        (om/build single-article (:current-page state))
-        (om/build page-bar state
-                           {:init-state {:current-page current-page}})
-      (apply dom/ul nil (om/build-all article (:articles state)
-                        {:init-state {:current-page current-page}}))))
-    om/IWillMount
-    (will-mount [_]
-    (do
-      (let [current-page (om/get-state owner :current-page)]
-        (go (loop []
-            (let [change (<! current-page)]
-                (if (= (:type change) "single")
-                (om/transact! state :current-page (fn [] (:value change)))
-                (om/transact! state :articles (fn [] (get change "articles")))
-                    ))(recur))))
-      (load-articles
-        (str "http://localhost:3000/blogposts?page=" (- (:page-number state) 1))
-        (fn [res]
+    (reify
+        om/IInitState
+        (init-state [_]
+            {:current-page (chan)})
+        om/IRenderState
+        (render-state [this {:keys [current-page]}]
+            (dom/div #js {:className "container"}
+            (apply dom/ul nil (om/build-all article (:articles state)
+                            {:init-state {:current-page current-page}}))
+            (om/build page-bar state
+                            {:init-state {:current-page current-page}})))
+        om/IWillMount
+        (will-mount [_]
             (do
-                (.log js/console (str "http://localhost:3000/blogposts?page=" (- (:page-number state) 1)))
-                (om/transact! state :articles (fn [] (get res "articles")))
-                (let [num-pages (/ (count (:articles state)) 5)]
-                    (om/transact! state :article-pages (fn [] num-pages))))))))))
+                (let [current-page (om/get-state owner :current-page)]
+                (go (loop []
+                    (let [change (<! current-page)]
+                        (if (= (:type change) "single")
+                            (om/transact! state :current-page (fn [] (:value change)))
+                            (om/transact! state :articles (fn [] (get change "articles")))
+                            ))(recur))))
+                (load-articles
+                    (str "http://localhost:3000/blogposts?page=" (- (:page-number state) 1))
+                        (fn [res]
+                            (do
+                                (om/transact! state :articles (fn [] (get res "articles")))
+                                (let [num-pages (/ (count (:articles state)) 5)]
+                                (om/transact! state :article-pages (fn [] num-pages))))))))))
 
 (defn main-page [state owner]
     (reify
@@ -122,8 +131,7 @@
                           :className ""}
             (dom/div #js {:className "container"}
                     (dom/h2 nil "Articles: ")
-                    (dom/h6 nil (str (:page-number state))
-                    (dom/div nil (om/build articles state))))))))
+                    (dom/div nil (om/build articles state)))))))
 
 (om/root main-page app-state
   {:target (. js/document (getElementById "app"))})
